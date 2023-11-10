3187ecaa79813093df5f56bab3879c34ba8fb8a6
Joelljunggren <joelljunggren@hotmail.com>
2023-11-10 13:51:00 +0100

    Rullande kommentarer på HOME. Css styling behövs fortfarande.

--- wwwroot/scripts/gallery.js
+++ wwwroot/scripts/gallery.js
@@ -1,4 +1,25 @@
-﻿function alertMe(url) {
-    console.log("Test");
-    console.log(url);
+﻿//function alertMe(url) {
+//    console.log("Test");
+//    console.log(url);
+//}
+
+var elem = document.querySelector('.main-gallery');
+if (elem) {
+    var flkty = new Flickity(elem, {
+        // options
+        cellAlign: 'center',
+        contain: true,
+        wrapAround: true,
+        autoPlay: 12000,
+        friction: 0.3,
+        selectedAttraction: 0.01,
+        prevNextButtons: false,
+        pageDots: false
+    });
+
+    // element argument can be a selector string
+    //   for an individual element
+    var flkty = new Flickity('.main-gallery', {
+        // options
+    });
 }
